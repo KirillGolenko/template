dd87fd15188547876a05605864e921aa2499fbc1
KirillGolenko <kgolenko.exccedteam@gmail.com>
2021-09-14 16:05:39 +0300

    create dynamic connections database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import User from '@users/entity/user.entity';
-import Product from 'src/products/entity/product.entity';
-import GoogleUser from '@users/entity/googleUser.entity';
 import * as config from 'config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: config.get('database.type'),
-      host: config.get('database.host'),
-      port: config.get('database.port'),
-      username: config.get('database.user'),
-      password: config.get('database.password'),
-      database: config.get('database.db'),
-      entities: [User, GoogleUser, Product],
-      synchronize: true,
+      ...config.get('database.postgresql'),
     }),
   ],
 })
