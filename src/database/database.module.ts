33450ceb18fa1a2ea57664af387ab539d0655256
KirillGolenko <kgolenko.exccedteam@gmail.com>
2021-09-14 17:06:12 +0300

    connect mongoose module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import * as config from 'config';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(config.get('database.mongodb.uri'), {
+      useFindAndModify: false,
+    }),
     TypeOrmModule.forRoot({
       ...config.get('database.postgresql'),
     }),
